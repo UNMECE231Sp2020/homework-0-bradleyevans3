21da18919e7adad7aa29a9f03c350f0ba37ad480
Bradley Evans <60158794+bradleyevans3@users.noreply.github.com>
2020-01-28 14:21:15 -0700

    Added comments that noted this program is being done in C++, not C. The C commands have been commented out.

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,11 @@
 # include <iostream>
 //# include <stdio.h>
+//<stdio.h> is good for C. However, we are doing this program in C++.
 
 int main() {
 	std::cout << "Hello world!" << std::endl;
 	//printf("Hello world!\n");
+	//The above command is good for printing "Hello world!" in C. However, we are doing this program in C++.
 	
 	return 0;
 }
-
